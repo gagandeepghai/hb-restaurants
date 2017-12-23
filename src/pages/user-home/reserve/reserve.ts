79fc9f556187823337d10b303a9232cfffea7f24
gagandeep ghai <gagandeepghai@gagandeeps-mbp.attlocal.net>
2017-12-23 18:26:02 -0500

    User API Changes

--- src/pages/user-home/reserve/reserve.ts
+++ src/pages/user-home/reserve/reserve.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { ViewController, AlertController } from 'ionic-angular';
 import { ViewChild } from '@angular/core';
 import { UserService } from '../../../providers/user-service';
 import { UserReward } from '../../../models/user-reward';
@@ -14,14 +14,31 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 export class UserReservation {
     constructor(private viewCtrl: ViewController,
                 private userService: UserService,
-                private callNumber: CallNumber) {
+                private callNumber: CallNumber, 
+                private alertCtrl: AlertController) {
       
     }
 
     call() {
-        this.callNumber.callNumber("+61289643389", true)
-        .then(() => console.log('Launched dialer!'))
-        .catch(() => console.log('Error launching dialer'));
+        let confirm = this.alertCtrl.create({
+            title: 'Call Punjaabs',
+            message: '+61 2 8964 3389',
+            buttons: [
+                {
+                text: 'Cancel',
+                handler: () => {}
+                },
+                {
+                text: 'Ok',
+                handler: () => {
+                        this.callNumber.callNumber("+61289643389", true)
+                        .then(() => console.log('Launched dialer!'))
+                        .catch(() => console.log('Error launching dialer'));
+                    }
+                }
+            ]
+            });
+            confirm.present();
     }
 
     dismiss(data): void {
